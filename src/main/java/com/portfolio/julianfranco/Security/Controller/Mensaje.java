3131aded10b6e297733b258425be2801992d7d7e
julianfran <julian_franco@hotmail.com.ar>
2023-03-21 18:50:32 -0300

    Versión 2

---
+++ src/main/java/com/portfolio/julianfranco/Security/Controller/Mensaje.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.julianfranco.Security.Controller;
+
+/**
+ *
+ * @author julian
+ */
+public class Mensaje {
+    private String mensaje;
+    
+    //Constructor
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    //Getters y setters
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
