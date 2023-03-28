b85783e4fd6da298ae2b3e7da99c53cd8269dfd9
julianfran <julian_franco@hotmail.com.ar>
2023-03-28 09:17:53 -0300

    Permisos para trabajar con firebase como con localhost - 2

--- src/main/java/com/portfolio/julianfranco/Controller/PersonaController.java
+++ src/main/java/com/portfolio/julianfranco/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "https://julianfrancofrontend.web.app")
+@CrossOrigin(origins = {"https://julianfrancofrontend.web.app","http://localhost:4200"})
 public class PersonaController {
       @Autowired
     ImpPersonaService personaService;
